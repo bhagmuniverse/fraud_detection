cd497f4b6cf10faeadc1863235324f80bc5e26b8
bhagmuniverse <bhag266@gmail.com>
2023-11-26 14:52:53 +0530

    Add data processing and ML model code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,14 @@
 print("adding code for the data processing")
+
+a =5
+b =7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print ("adding multiplication function")
+
+def mul(a,b):
+    c = a*b
+    return c
--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,3 @@
 print ("creating a new ml model")
-
+print ("Git in IDE")
 print("this is the final working model")
